00f10f5e2d4bdd3b9b9c16587efafc3599a16549
Artsem <bobrovnik42@gmail.com>
2024-04-03 02:50:07 +0300

    fix copy email (clipbord API didn't work on Ubuntu)

--- CatFactory/static/js/copy_email_btn.js
+++ CatFactory/static/js/copy_email_btn.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btns = document.getElementsByClassName('email-btn');
     const arrBtns = Array.from(btns);
-    arrBtns.forEach(btn => 
+
+    arrBtns.forEach(btn => {
         btn.addEventListener('click', (e) => {
             const { target } = e;
-            navigator.clipboard.writeText(target.attributes['email'].nodeValue)
-        }))
-})
+            const email = String(target?.attributes?.['email']?.nodeValue);
+            navigator?.clipboard?.writeText(email) || copyTextToClipboard(email);
+        });
+    });
+
+    // On Linux can't use property clipboard and use this func
+    function copyTextToClipboard(text) {
+        let textarea = document.createElement("textarea");
+        textarea.value = text;
+        document.body.appendChild(textarea);
+
+        textarea.select();
+        textarea.setSelectionRange(0, textarea.value.length);
+
+        document.execCommand("copy");
+
+        document.body.removeChild(textarea);
+    }
+});
